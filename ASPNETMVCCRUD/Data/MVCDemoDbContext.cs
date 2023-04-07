2217854a35a7b488119ce9bb5953c2d1eaf470fb
rani9 <rani9@PRINCE-ROG1>
2023-04-07 09:34:33 -0700

    Add project files.

---
+++ ASPNETMVCCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ASPNETMVCCRUD.Models.Domain;
+
+namespace ASPNETMVCCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
